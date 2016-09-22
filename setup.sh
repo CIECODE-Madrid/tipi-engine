1262a293ad4530a589e4f97bc35b9fa2adb795c8
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-22 13:50:21 +0200

    change libs

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ fi
 
 
 
-sudo apt-get update && sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libxml2-dev libxslt1-dev zlib1g-dev libssl-dev virtualenv
+sudo apt-get update && sudo apt-get install -y  build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libxml2-dev libxslt1-dev zlib1g-dev libssl-dev virtualenv libpcre3 libpcre3-dev
 
 
 if [ ! -d "engineenv" ]; then
