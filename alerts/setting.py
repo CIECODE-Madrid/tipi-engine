12e75e9d50db15391657d18f92c00a9459304f0f
pr3ssh <pablo.martin@enreda.coop>
2016-09-25 14:07:29 +0200

    Update alerts on emails

--- alerts/setting.py
+++ alerts/setting.py
@@ -1,3 +1,3 @@
-DOMAIN="tipi.es"
+DOMAIN="http://tipiciudadano.es/tipis/"
 API_SPARKPOST="f70be647f745f194cc0ccf68ee88ad96b10e3e17"
-FROM_EMAIL="sandbox@sparkpostbox.com"
+FROM_EMAIL="tipi@ciecode.es"
