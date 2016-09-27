bad1974de3c653a711b79ec61bec343eac5cfe5f
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-27 08:09:55 +0200

    fixed path

--- cron.sh
+++ cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Init Luigi at  $(date)." >> /var/log/luigi
+echo "Init Luigi at  $(date)." >> /var/log/luigi/processes.log
 cd /home/enreda/tipi-engine
 source engineenv/bin/activate
 python base.py
 
-echo "Finish luigi at $(date). ">> /var/log/luigi
-echo "****************">> /var/log/luigi
+echo "Finish luigi at $(date). ">> /var/log/luigi/processes.log
+echo "****************">> /var/log/luigi/processes.log
