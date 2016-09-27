bac71331eae6ac550c8273476f41a88c9576c8b6
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-27 08:08:05 +0200

    added path log for init and finish processes

--- cron.sh
+++ cron.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Init Luigi at  $(date)." >> /var/log/luigi
 cd /home/enreda/tipi-engine
 source engineenv/bin/activate
 python base.py
+
+echo "Finish luigi at $(date). ">> /var/log/luigi
+echo "****************">> /var/log/luigi
