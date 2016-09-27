a68df732b38935b1ef1042e057790e806ad2b304
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-27 08:12:06 +0200

    added sudo

--- cron.sh
+++ cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Init Luigi at  $(date)." >> /var/log/luigi/processes.log
+sudo echo "Init Luigi at  $(date)." >> /var/log/luigi/processes.log
 cd /home/enreda/tipi-engine
 source engineenv/bin/activate
 python base.py
 
-echo "Finish luigi at $(date). ">> /var/log/luigi/processes.log
-echo "****************">> /var/log/luigi/processes.log
+sudo echo "Finish luigi at $(date). ">> /var/log/luigi/processes.log
+sudo echo "****************">> /var/log/luigi/processes.log
