460684d09238132ebb843724da57631758303637
pr3ssh <pablo.martin@enreda.coop>
2017-07-03 11:40:35 +0200

    Ignoring files

--- database/variables.py
+++
@@ -1,9 +0,0 @@
-config = {
-    # MONGO CONFIG
-    "MONGO_USERNAME": None,
-    "MONGO_PASSWORD": None,
-    "MONGO_HOST": "localhost",
-    "MONGO_PORT": 3001,
-    "MONGO_DB_NAME": "meteor",
-    # Other config follows
-}
