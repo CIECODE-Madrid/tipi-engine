7851312fe057ab712ef798dda29190d616b092b1
pr3ssh <pablo.martin@enreda.coop>
2017-07-03 13:39:13 +0200

    Update send emails method

--- alerts/send_email.py
+++ alerts/send_email.py
@@ -1,4 +1,3 @@
-import pdb
 from sparkpost import SparkPost
 from settings import API_SPARKPOST, FROM_NAME, FROM_EMAIL
 from jinja2 import Environment, FileSystemLoader
@@ -11,11 +10,12 @@ class emailSparkPost(object):
         sp = SparkPost(API_SPARKPOST)
         doc = render_html_doc(listforuser)
         response = sp.transmissions.send(
-            recipients=[email],#email is a user's email
-            html=doc,
-            from_email=FROM_EMAIL,
-            from_name=FROM_NAME,
-            subject='Hay nuevos Tipis para ti'
+            recipients = [email], #email is a user's email
+            html = doc,
+            from_email = FROM_EMAIL,
+            subject = 'Hay nuevos Tipis para ti',
+            track_opens = True,
+            track_clicks = True
         )
         print "Sending email to "+email
 
