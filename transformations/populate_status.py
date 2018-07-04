217588e3b216d302e6096038f978e2f2c4aa4384
pr3ssh <x@pr3ssh.net>
2018-07-04 16:07:52 +0200

    Update status map for populating initiative status

--- transformations/populate_status.py
+++ transformations/populate_status.py
@@ -15,22 +15,5 @@ class PopulateStatus:
 
     def populate(self):
         dbmanager = Congress()
-        for status in STATUS_MAP.keys():
-            status_keys = STATUS_MAP[status].keys()
-            status_search = dict()
-            for status_key in status_keys:
-                aux_status_key = status_key
-                if aux_status_key is 'processing':
-                    aux_status_key = '$or'
-                status_search[aux_status_key] = self._process_status_key(status, status_key)
-            dbmanager.updateInitiativesStatus(status_search, status)
-
-    def _process_status_key(self, status, status_key):
-        if status_key is 'processing':
-            processing_array = []
-            for processing in STATUS_MAP[status][status_key]:
-                processing_array.append(
-                        {'processing': {'$regex': processing, '$options': 'gi'}}
-                    )
-            return processing_array
-        return {'$in': STATUS_MAP[status][status_key]}
+        for status_element in STATUS_MAP:
+            dbmanager.updateInitiativesStatus(status_element['search'], status_element['status'])
--- transformations/status_map.py
+++ transformations/status_map.py
@@ -1,98 +1,72 @@
 # -*- coding: utf-8 -*-
 
-STATUS_MAP = {
-        'Aprobada': {
-            'processing': [
-                "Aprobado con modificaciones",
-                "Aprobado sin modificaciones",
-                "Convalidado",
-                "Tramitado por completo sin",
-                "Concluido.*Aprobado con modificaciones.*desde.*",
-                ],
-            "initiative_type_alt": [
-                "Información sobre Convenios Internacionales",
-                "Otros asuntos relativos a Convenios Internacionales",
-                "Real Decreto Legislativo",
-                "Declaración Institucional",
-                "Solicitud de Comparecencia ante el Pleno",
-                "Solicitud de comparecencia en Comisión",
-                "Solicitud de comparecencia de autoridades y funcionarios en Comisión",
-                "Solicitud de comparecencia del Gobierno en Comisión",
-                "Solicitud de Comparecencia en Comisión"
-                ]
-            },
-        "Rechazada": {
-            "processing": [
-                "Rechazado"
-                ]
-            },
-        "Respondida": {
-            "processing": [
-                "Tramitado por completo sin",
-                "Gobierno Reclamación"
-                ],
-            "initiative_type_alt": [
-                "Interpelación urgente",
-                "Pregunta oral en Pleno",
-                "Pregunta oral al Gobierno en Comisión",
-                "Pregunta al Gobierno con respuesta escrita",
-                "Respuesta"
-                ]
-            },
-        "Celebrada": {
-            "processing": [
-                "Tramitado por completo sin"
-                ],
-            "initiative_type_alt": [
-                "Comparecencia en Comisión",
-                "Comparecencia de autoridades y funcionarios en Comisión",
-                "Comparecencia del Gobierno en Comisión",
-                "Otras comparecencias en Comisión"
-                ]
-            },
-        "En tramitación": {
-            "processing": [
-                "Boletín Oficial de las Cortes Generales Publicación desde",
-                "Comisión.*desde",
-                "Concluido desde",
-                "Gobierno Contestación",
-                "Junta de Portavoces",
-                "Mesa del Congreso Acuerdo",
-                "Mesa del Congreso Requerimiento",
-                "Mesa del Congreso Calificación",
-                "Mesa del Congreso Reclamación",
-                "Pleno Aprobación desde",
-                "Pleno desde",
-                "Pleno Toma en consideración",
-                "Solicitud de amparo",
-                "Respuesta.*Gobierno",
-                "Senado desde",
-                ],
-            },
-        "No admitida a trámite": {
-                "processing": [
-                    "Inadmitido a trámite"
-                    ]
-                },
-        "No debatida": {
-                "processing": [
-                    "Decaído"
-                    ]
-                },
-        "Retirada": {
-                "processing": [
-                    "Retirado",
-                    "Extinguido por desaparición o cese del autor"
-                    ]
-                },
-        "Convertida en otra": {
-                "processing": [
-                    "Convertido"
-                    ]
-                },
-        "Acumulada en otra": {
-                "processing": [
-                    "Subsumido en otra iniciativa"
-                    ]
-                }
+STATUS_MAP = [
+        {
+            'search': {'processing': {'$regex': 'Aprobado con modificaciones', '$options': 'gi'}},
+            'status': 'Aprobada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Aprobado sin modificaciones', '$options': 'gi'}},
+            'status': 'Aprobada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Convalidado', '$options': 'gi'}},
+            'status': 'Aprobada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Tramitado por completo sin', '$options': 'gi'}, 'initiative_type': {'$in': ["171", "180", "181", "184"]}},
+            'status': 'Respondida'
+        },
+        {
+            'search': {'processing': {'$regex': 'Tramitado por completo sin', '$options': 'gi'}, 'initiative_type': {'$in': ["212", "213", "214", "219"]}},
+            'status': 'Celebrada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Tramitado por completo sin', '$options': 'gi'}, 'initiative_type': {'$not': {'$in': ["171", "180", "181", "184", "212", "213", "214", "219"]}}},
+            'status': 'Aprobada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Convertido', '$options': 'gi'}},
+            'status': 'Convertida en otra'
+        },
+        {
+            'search': {'$or': [
+                {'processing': {'$regex': "Boletín Oficial de las Cortes Generales Publicación desde", '$options': 'gi'}},
+                {'processing': {'$regex': "Comisión.*desde", '$options': 'gi'}},
+                {'processing': {'$regex': "Concluido desde", '$options': 'gi'}},
+                {'processing': {'$regex': "Gobierno Contestación", '$options': 'gi'}},
+                {'processing': {'$regex': "Junta de Portavoces", '$options': 'gi'}},
+                {'processing': {'$regex': "Mesa del Congreso Acuerdo", '$options': 'gi'}},
+                {'processing': {'$regex': "Mesa del Congreso Requerimiento", '$options': 'gi'}},
+                {'processing': {'$regex': "Mesa del Congreso Calificación", '$options': 'gi'}},
+                {'processing': {'$regex': "Mesa del Congreso Reclamación", '$options': 'gi'}},
+                {'processing': {'$regex': "Pleno Aprobación desde", '$options': 'gi'}},
+                {'processing': {'$regex': "Pleno desde", '$options': 'gi'}},
+                {'processing': {'$regex': "Pleno Toma en consideración", '$options': 'gi'}},
+                {'processing': {'$regex': "Solicitud de amparo", '$options': 'gi'}},
+                {'processing': {'$regex': "Respuesta.*Gobierno", '$options': 'gi'}},
+                {'processing': {'$regex': "Senado desde", '$options': 'gi'}}
+                ]},
+            'status': 'En tramitación'
+        },
+        {
+            'search': {'processing': {'$regex': 'Subsumida en otra iniciativa', '$options': 'gi'}},
+            'status': 'Acumulada en otra'
+        },
+        {
+            'search': {'processing': {'$regex': 'Inadmitido a trámite', '$options': 'gi'}},
+            'status': 'No admitida a trámite'
+        },
+        {
+            'search': {'processing': {'$regex': 'Decaído', '$options': 'gi'}},
+            'status': 'No debatida'
+        },
+        {
+            'search': {'processing': {'$regex': 'Rechazado', '$options': 'gi'}},
+            'status': 'Rechazada'
+        },
+        {
+            'search': {'processing': {'$regex': 'Retirado', '$options': 'gi'}},
+            'status': 'Retirada'
         }
+    ]
