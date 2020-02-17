8731fea95e04bf65676e7e05ea528260c65fa819
Víctor Ramírez de la Corte <virako.9@gmail.com>
2020-02-17 08:13:19 +0100

    Add log with sentry

--- base.py
+++ base.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python
 
+from os import environ as env
+
 import luigi
+import sentry_sdk
+
 from stats.stats import GenerateStatsTask
+
+
+SENTRY_DSN = env.get('SENTRY_DSN', None)
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN)
 
 
 if __name__ == '__main__':
