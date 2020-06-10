6e3584703021751c863f5b72117520aa2afe82aa
pr3ssh <x@pr3ssh.net>
2020-06-10 16:47:51 +0200

    Add Paraguay's mimetype attachments

---
+++ extractors/paraguay/initiatives_attachments.py
@@ -0,0 +1,12 @@
+MIMETYPE_FILE_EXTENSIONS = {
+        'application/msword': '.doc',
+        'application/pdf': '.pdf',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
+        'application/vnd.ms-excel': '.xls',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': '.xlsx',
+        'application/vnd.ms-powerpoint': '.ppt',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation': '.pptx',
+        'application/vnd.oasis.opendocument.text': '.odt',
+        'application/vnd.oasis.opendocument.spreadsheet': '.ods',
+        'application/vnd.oasis.opendocument.presentation': '.odp'
+        }
