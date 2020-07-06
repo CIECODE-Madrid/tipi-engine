707a6e881fb45d4bc7175e8da334c9974d9211db
pr3ssh <x@pr3ssh.net>
2020-07-06 20:21:20 +0200

    Refactoring Blacklist (+ flake8 support)

--- extractors/spain/scrap/blacklist.py
+++ extractors/spain/scrap/blacklist.py
@@ -1,13 +1,15 @@
-#singleton implemented
+# singleton implemented
 import re
 import sys
 from pathlib import Path
-p = Path(__file__).parents[3]
-sys.path.insert(0,str(p))
 
 import redis
 
 from extractors.config import REDIS_DB_BLACKLIST, REDIS_HOST, REDIS_PORT
+
+
+p = Path(__file__).parents[3]
+sys.path.insert(0, str(p))
 
 
 class ManageRedisBlackList(object):
@@ -15,47 +17,43 @@ class ManageRedisBlackList(object):
     _conn = None
 
     def __init__(self):
-        self._conn = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_BLACKLIST)
+        self._conn = redis.Redis(
+                host=REDIS_HOST,
+                port=REDIS_PORT,
+                db=REDIS_DB_BLACKLIST)
 
     def __new__(cls):
-        if cls.__instance == None:
+        if cls.__instance is None:
             cls.__instance = object.__new__(cls)
             cls.__instance.name = "Reading..."
         return cls.__instance
 
-
-    def getElement(self,key):
+    def getElement(self, key):
         return self._conn.get(key)
 
-    def addElement(self,key):
-        return self._conn.set(key,key)
+    def addElement(self, key):
+        return self._conn.set(key, key)
 
 
 class Blacklist():
 
     @staticmethod
     def getElement(key):
-        file = ManageRedisBlackList()
-
-        return file.getElement(key)
+        return ManageRedisBlackList().getElement(key)
 
     @staticmethod
     def addElement(key):
-        file = ManageRedisBlackList()
-
-        return file.addElement(key)
-
+        return ManageRedisBlackList().addElement(key)
 
     @staticmethod
-    def isAddedtolist(line):
-        rexps= ["caducad(a|o)","rechazad(a|o)","aprobad(a|o)","subsumid(o|a)"
-                ,"inadmitid(o|a)","concluid(o|a)","retirad(o|a)"]
+    def isFinalState(line):
+        regex_list = ["caducad(a|o)", "rechazad(a|o)", "aprobad(a|o)",
+                "subsumid(o|a)", "inadmitid(o|a)", "concluid(o|a)",
+                "retirad(o|a)"]
 
         control = False
-        for rege in rexps:
-            if rege is "tramitad(o|a)":
-                pass
-            if re.search(rege,line,re.IGNORECASE):
-                control= True
+        for regex in regex_list:
+            if re.search(regex, line, re.IGNORECASE):
+                control = True
                 break
         return control
--- extractors/spain/scrap/pipelines.py
+++ extractors/spain/scrap/pipelines.py
@@ -5,19 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
-import pymongo
-
-from scrapy.conf import settings
-from scrapy.exceptions import DropItem
-from scrapy import log
-import json
-
 from scrap.blacklist import Blacklist
-from scrap.items import InitiativeItem, FinishTextItem, AmendmentItem,ResponseItem
-from scrap.utils import Utils
-from database.congreso import Congress
+from scrap.items import InitiativeItem,\
+                        FinishTextItem,\
+                        AmendmentItem,\
+                        ResponseItem
 
+from database.congreso import Congress
 
 
 class MongoDBPipeline(object):
@@ -27,63 +21,72 @@ class MongoDBPipeline(object):
         if isinstance(item, InitiativeItem):
 
             try:
-                if Blacklist.isAddedtolist(item['tramitacion']):
-                        Blacklist.addElement(item['url'])
+                if Blacklist.isFinalState(item['processing']):
+                    Blacklist.addElement(item['url'])
 
-            except:
-                print("**** FAILURE *****")
+            except Exception as e:
+                print("**** FAILURE: {} *****".format(e))
             congress = Congress()
-            search = congress.getInitiative(reference=item['reference'], initiative_type_alt=item['initiative_type_alt'], title=item['title'])
+            search = congress.getInitiative(
+                    reference=item['reference'],
+                    initiative_type_alt=item['initiative_type_alt'],
+                    title=item['title'])
 
             if congress.isDiffInitiative(item=item, search=search):
-                    #actualizar
+                # Update
                 congress.updateorinsertInitiativecontent(item=item, type='update')
 
             elif not search:
-                    #no existe
-                    #insertar sin mas
+                # Does not exist (just insert)
                 congress.updateorinsertInitiativecontent(item=item, type='insert')
             else:
-                        #not DIFF
-                print("es el mismo, no cambia")
-
+                # It is the same initiative
+                print("It is the same, nothing change")
 
 
         elif isinstance(item, AmendmentItem):
             congress = Congress()
-            search = congress.getAdmendment(reference=item['reference'], initiative_type_alt=item['initiative_type_alt'], parliamentarygroup=item['author_parliamentarygroups'])
+            search = congress.getAdmendment(
+                    reference=item['reference'],
+                    initiative_type_alt=item['initiative_type_alt'],
+                    parliamentarygroup=item['author_parliamentarygroups'])
 
-            #esta de otra forma estructurado
+            # Esta de otra forma estructurado
             congress.updateorinsertAdmenment(item=item, search=search)
+
         elif isinstance(item, FinishTextItem):
             congress = Congress()
-            search = congress.getInitiative(reference=item['reference'], initiative_type_alt=item['initiative_type_alt'], title=item['title'])
+            search = congress.getInitiative(
+                    reference=item['reference'],
+                    initiative_type_alt=item['initiative_type_alt'],
+                    title=item['title'])
             if congress.isDiffInitiative(item=item, search=search):
-                #actualizar
+                # Update
                 congress.updateorinsertFinishtextorResponse(item=item, type='update')
 
             elif not search:
-                #no existe
-                #insertar sin mas
+                # Does not exist (just insert)
                 congress.updateorinsertFinishtextorResponse(item=item, type='insert')
             else:
-                    #not DIFF
-                print("es el mismo, no cambia")
+                # It is the same initiative
+                print("It is the same, nothing change")
 
         elif isinstance(item, ResponseItem):
             congress = Congress()
-            search = congress.getInitiative(reference=item['reference'], initiative_type_alt=item['initiative_type_alt'], title=item['title'])
+            search = congress.getInitiative(
+                    reference=item['reference'],
+                    initiative_type_alt=item['initiative_type_alt'],
+                    title=item['title'])
             if congress.isDiffInitiative(item=item, search=search):
-                #actualizar
+                # Update
                 congress.updateorinsertFinishtextorResponse(item=item, type='update')
 
             elif not search:
-                #no existe
-                #insertar sin mas
+                # Does not exist (just insert)
                 congress.updateorinsertFinishtextorResponse(item=item, type='insert')
             else:
-                    #not DIFF
-                print("es el mismo, no cambia")
+                # It is the same initiative
+                print("It is the same, nothing change")
 
         return item
 
