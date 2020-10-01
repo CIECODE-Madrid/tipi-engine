39af52d9165612c36cb507f4d525a496e8d99c16
Daniel Lledó Raigal <dani@daniMac.local>
2020-10-01 10:09:55 +0200

    load-more-deputy-data

--- extractors/spain/scrap/spiders/members.py
+++ extractors/spain/scrap/spiders/members.py
@@ -53,9 +53,17 @@ class MemberSpider(CrawlSpider):
         extra_data = Selector(response).xpath('//div[@class="webperso_dip"]/div/a/@href')
         avatar = Selector(response).xpath('//div[@id="datos_diputado"]/p[@class="logo_g'
                           'rupo"]/img[@name="foto"]/@src').extract()
-
+        constituency = Selector(response).xpath('//div[@class="texto_dip"]/ul/li[1]/div[1][@class="dip_rojo"]').css('::text').extract()[0].strip().split()[2][:-1]
+        resources = Selector(response).xpath("//ul/li[@class='regact_dip']").css('a::attr(href)').extract()
+        congress_position = Selector(response).xpath("//p[@class='pos_hemiciclo']/img").css('::attr(src)').extract()
+        public_charges = Selector(response).xpath('(//div[@class = "listado_1"])[1]/ul/li').extract()
+        birthday = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[1]').extract()
+        legislatures = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[2]').extract()
+        personal_information = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[3]').extract()
+        social_networks = Selector(response).xpath('//div[@class="webperso_dip"]/div[@class="webperso_dip_imagen"]/a/@href')
+        party_logo = Selector(response).xpath('(//p[@class = "logo_grupo"])[2]/a/img/@src').extract()
         item = MemberItem()
-
+        
         item['url'] = response.url
         item['name'] = ""
         item['image'] = ""
@@ -65,13 +73,79 @@ class MemberSpider(CrawlSpider):
         item['web'] = ""
         item['email'] = ""
         item['twitter'] = ""
+        item['facebook'] = ""
+        #item['instagram'] = ""
         item['active'] = True
+        item['constituency'] = ""
+        item['activity_resource'] = ""
+        item['assets_resource'] = ""
+        item['public_charges'] = []
+        item['birthday'] = ""
+        item['legislatures'] = ""
+        item['personal_information']=[]
+        item['party_logo']=""
+        
 
         if names:
             second_name, name = names[0].split(',')
             item['name'] = second_name.strip()+", "+name.strip()
+            if constituency:
+                item['constituency'] = constituency
+            if len(resources)>0:
+                item['activity_resource'] = 'http://www.congreso.es' + resources[0]
+            if len(resources)>1:
+                item['assets_resource'] = 'http://www.congreso.es' + resources[1]
             if avatar:
                 item['image'] = 'http://www.congreso.es' + avatar[0]
+            if public_charges: 
+                resu = []
+                for s in public_charges:
+                    regex = re.compile(r'[\n\r\t]')
+                    res = regex.sub(" ",s)
+                    res = res.replace("<li>","").replace("</li>","").replace("  ","").strip()
+                    ini = res[:res.find('<a')] 
+                    fin = res[res.find('class')+9:res.find('</a')]
+                    string = ini + fin
+                    resu.append(string)
+                item['public_charges'] = resu
+            if birthday:
+                resu = []
+                for s in birthday:
+                    regex = re.compile(r'[\n\r\t]')
+                    res = regex.sub(" ",s)
+                    res = res.replace("<li>","").replace("</li>","").replace("  ","").replace(u'\xa0', u' ').strip()
+                    resu.append(res)
+                item['birthday'] = resu[0]
+            if legislatures:
+                resu = []
+                for s in legislatures:
+                    regex = re.compile(r'[\n\r\t]')
+                    res = regex.sub(" ",s)
+                    res = res.replace("<li>","").replace("</li>"," ").replace(u'\xa0', u' ').replace("   ","").strip()
+                    resu.append(res)
+                item['legislatures']= resu[0]
+            if personal_information:
+                resu = []
+                for s in personal_information:
+                    regex = re.compile(r'[\n\r\t]')
+                    res = regex.sub(" ",s)
+                    res = res.replace("<li>","").replace("</li>"," ").replace("\\xa0"," ").replace("   ","").strip()
+                    resu = res.split('<br>')
+                item['personal_information'] = resu
+            if len(social_networks)>0:
+                for net in social_networks:
+                    twitter = net.re('[http|https]*://(?:twitter.com)/[\w]*')
+                    if twitter:
+                        item['twitter'] = twitter[0]
+                    facebook = net.re('[http|https]*://(?:www.facebook.com)/[\w]*')
+                    if facebook:
+                        item['facebook'] = facebook[0]
+                    #instagram = net.re('[http|https]*://(?:www.instagram.com)/[\w]*')
+                    #if instagram:
+                     #   item['instagram'] = instagram[0]
+            if party_logo:
+                item['party_logo'] = "http://congreso.es" + party_logo[0]
+
             if curriculum:
 
                 group = curriculum.xpath('a/text()')
@@ -106,9 +180,9 @@ class MemberSpider(CrawlSpider):
                 email = extra_data.re('mailto:[\w.-_]*@[\w.-_]*')
                 if email:
                     item['email'] = email[0].replace('mailto:', '')
-                twitter = extra_data.re('[http|https]*://(?:twitter.com)/[\w]*')
-                if twitter:
-                    item['twitter'] = twitter[0]
+                #twitter = extra_data.re('[http|https]*://(?:twitter.com)/[\w]*')
+                #if twitter:
+                 #   item['twitter'] = twitter[0]
         congress = Congress()
         search = congress.getDeputy(name=item['name'])
         if not search:
