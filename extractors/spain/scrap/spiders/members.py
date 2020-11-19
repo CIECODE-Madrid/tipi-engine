dafa5161029627bdbfdace7a5b2f5450910b4574
pr3ssh <x@pr3ssh.net>
2020-11-19 16:15:05 +0100

    Fixes deputies without birthdates + Substitutes deprecated function + Flake8 recommendations

--- extractors/spain/scrap/spiders/members.py
+++ extractors/spain/scrap/spiders/members.py
@@ -1,24 +1,24 @@
-from urllib.parse import urlparse
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.selector import HtmlXPathSelector, Selector
-from scrapy.item import Item, Field
 import re
-from dateutil.parser import parse
 from datetime import datetime
+from dateutil.parser import parse
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+
+from scrap.items import MemberItem
 from database.congreso import Congress
 from extractors.config import ID_LEGISLATURA
-from scrap.items import MemberItem
 
 
 class MemberSpider(CrawlSpider):
     name = 'members'
     allowed_domains = ['congreso.es', ]
-    start_urls = [  'http://www.congreso.es/portal/page/portal/Congreso'
-                        '/Congreso/Diputados?_piref73_1333056_73_1333049_13'
-                        '33049.next_page=/wc/menuAbecedarioInicio&tipoBusqu'
-                        'eda=completo&idLegislatura={}'.format(ID_LEGISLATURA),
-                    'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BajasLegAct']
+    start_urls = ['http://www.congreso.es/portal/page/portal/Congreso'
+            '/Congreso/Diputados?_piref73_1333056_73_1333049_13'
+            '33049.next_page=/wc/menuAbecedarioInicio&tipoBusqu'
+            'eda=completo&idLegislatura={}'.format(ID_LEGISLATURA),
+            'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/BajasLegAct']
 
     rules = []
     rules.append(
@@ -32,39 +32,42 @@ class MemberSpider(CrawlSpider):
                        '&tipoBusqueda=completo'.format(ID_LEGISLATURA)], unique=True), follow=True))
     rules.append(
             Rule(LinkExtractor(
-                allow=['diputadosBajaLegActual&paginaActual=\d']
-                    , unique=True), follow=True))
+                allow=['diputadosBajaLegActual&paginaActual=\d'], unique=True), follow=True))
 
-    def text_cleaner(self,text):
+    def text_cleaner(self, text):
         regex = re.compile(r'[\n\r\t]')
-        res = regex.sub(" ",text)
-        res = res.replace("<li>","").replace("</li>","").replace("  ","").replace(u'\xa0', u' ').strip()
+        res = regex.sub(" ", text)
+        res = res.replace("<li>", "").replace("</li>", "").replace("  ", "").replace(u'\xa0', u' ').strip()
         string = res.split(" ")
         res = ""
         for s in string:
             res += s + " "
         res = res.strip()
         return res
+
     def leg_cleaner(self,text):
         regex = re.compile(r'[\n\r\t]')
-        text = text.replace("Diputado","").replace("Diputada","").replace("de la","").replace("legislatura.","").replace("legislaturas.","").replace("y",",")
+        text = text.replace("Diputado", "").replace("Diputada", "").replace("de la", "").replace("legislatura.", "").replace("legislaturas.", "").replace("y", ",")
         res = regex.sub(" ",text)
-        res = res.replace("<li>","").replace("</li>","").replace("  ","").replace(u'\xa0', u' ').strip()
+        res = res.replace("<li>", "").replace("</li>", "").replace("  ", "").replace(u'\xa0', u' ').strip()
         res = res.replace(' ', '').split(",")
         return res
-    def birthdate_extract(self,text):
-        res = []
-        day = int(re.findall('\s+([0-9]{1,2})\s',text)[0])
-        month = self.month_parser(re.findall('(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)',text)[0])
-        year = int(re.findall('([0-9]{4})',text)[0])
-        date = datetime(year,month,day)
-        return date
-    def date_extractor(self,datetext):
-        date = re.findall(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", datetext)[0]
-        date = datetime.strptime(date,'%d/%m/%Y')
+
+    def birthdate_extract(self, text, name):
+        day = int(re.findall('\s+([0-9]{1,2})\s', text)[0])
+        month = self.month_parser(re.findall(
+            '(enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)',
+            text)[0])
+        year = int(re.findall('([0-9]{4})', text)[0])
+        date = datetime(year, month, day)
         return date
 
-    def month_parser(self,month):
+    def date_extractor(self, datetext):
+        date = re.findall(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", datetext)[0]
+        date = datetime.strptime(date, '%d/%m/%Y')
+        return date
+
+    def month_parser(self, month):
         switcher = {
             "enero": 1,
             "febrero": 2,
@@ -81,29 +84,25 @@ class MemberSpider(CrawlSpider):
         }
         return switcher[month]
 
-
-
-
     def parse_member(self, response):
-
-
         # extract full name of member
         names = Selector(response).xpath('//div[@class="nombre_dip"]/text()').extract()
         # extra text like member's state
-        curriculum = Selector(response).xpath('//div[@class="texto_dip"]/ul/li/div[@class="dip'
-                              '_rojo"]')
+        curriculum = Selector(response).xpath('//div[@class="texto_dip"]/ul/li/div[@class="dip_rojo"]')
         start_date = Selector(response).xpath('(//div[@class="texto_dip"]/ul/li/div[@class="dip_rojo"])[3]').extract()[0]
-        # email, twitter ....
         extra_data = Selector(response).xpath('//div[@class="webperso_dip"]/div/a/@href')
-        avatar = Selector(response).xpath('//div[@id="datos_diputado"]/p[@class="logo_g'
-                          'rupo"]/img[@name="foto"]/@src').extract()
+        avatar = Selector(response).xpath('//div[@id="datos_diputado"]/p[@class="logo_grupo"]/img[@name="foto"]/@src').extract()
         constituency = Selector(response).xpath('//div[@class="texto_dip"]/ul/li[1]/div[1][@class="dip_rojo"]').css('::text').extract()[0].strip().split()[2]
         resources = Selector(response).xpath("//ul/li[@class='regact_dip']").css('a::attr(href)').extract()
-        congress_position = Selector(response).xpath("//p[@class='pos_hemiciclo']/img").css('::attr(src)').extract()
         public_position = Selector(response).xpath('(//div[@class = "listado_1"])[1]/ul/li').extract()
-        birthdate = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[1]').extract()
-        legislatures = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[2]').extract()
-        bio = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[3]').extract()
+        birthdate = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[1]').extract()[0]
+        next_index = 2
+        # There are some deputies without birth dates
+        if 'Nacid' not in birthdate:
+            birthdate = ''
+            next_index = 1
+        legislatures = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[{}]'.format(next_index)).extract()
+        bio = Selector(response).xpath('((//div[@class="texto_dip"])[2]/ul/li)[{}]'.format(next_index+1)).extract()
         social_networks = Selector(response).xpath('//div[@class="webperso_dip"]/div[@class="webperso_dip_imagen"]/a/@href')
         party_logo = Selector(response).xpath('(//p[@class = "logo_grupo"])[2]/a/img/@src').extract()
         item = MemberItem()
@@ -118,37 +117,30 @@ class MemberSpider(CrawlSpider):
         item['email'] = ""
         item['twitter'] = ""
         item['facebook'] = ""
-        #item['instagram'] = ""
         item['active'] = True
         item['constituency'] = ""
-        #item['activity_resource'] = ""
-        #item['assets_resource'] = ""
         item['public_position'] = []
         item['birthdate'] = ""
         item['legislatures'] = ""
-        item['bio']=[]
-        item['party_logo']=""
-        item['extra']={}
-        item['extra']['activity_resource']=""
-        item['extra']['assets_resource']=""
-
+        item['bio'] = []
+        item['party_logo'] = ""
+        item['extra'] = dict()
+        item['extra']['activity_resource'] = ""
+        item['extra']['assets_resource'] = ""
 
         if names:
             second_name, name = names[0].split(',')
-            item['name'] = second_name.strip()+", "+name.strip()
+            item['name'] = second_name.strip()+", " + name.strip()
             if constituency:
-                item['constituency'] = constituency.replace(".","")
-            if len(resources)>0:
+                item['constituency'] = constituency.replace(".", "")
+            if len(resources) > 0:
                 item['extra']['activity_resource'] = 'http://www.congreso.es' + resources[0]
-            if len(resources)>1:
+            if len(resources) > 1:
                 item['extra']['assets_resource'] = 'http://www.congreso.es' + resources[1]
             if avatar:
                 item['image'] = 'http://www.congreso.es' + avatar[0]
             if birthdate:
-                for s in birthdate:
-                    res = self.text_cleaner(s)
-                    birthdate_data = self.birthdate_extract(res)
-                item['birthdate'] = birthdate_data
+                item['birthdate'] = self.birthdate_extract(self.text_cleaner(birthdate), item['name'])
             if start_date:
                 result = self.date_extractor(start_date)
                 item['start_date'] = result
@@ -164,7 +156,7 @@ class MemberSpider(CrawlSpider):
             if legislatures:
                 for s in legislatures:
                     res = self.leg_cleaner(s)
-                item['legislatures']=res
+                item['legislatures'] = res
             if bio:
                 resu = []
                 for s in bio:
@@ -175,7 +167,7 @@ class MemberSpider(CrawlSpider):
                     s = s.strip()
                     fin.append(s)
                 item['bio'] = fin
-            if len(social_networks)>0:
+            if len(social_networks) > 0:
                 for net in social_networks:
                     twitter = net.re('[http|https]*://(?:twitter.com)/[\w]*')
                     if twitter:
@@ -183,9 +175,6 @@ class MemberSpider(CrawlSpider):
                     facebook = net.re('[http|https]*://(?:www.facebook.com)/[\w]*')
                     if facebook:
                         item['facebook'] = facebook[0]
-                    #instagram = net.re('[http|https]*://(?:www.instagram.com)/[\w]*')
-                    #if instagram:
-                     #   item['instagram'] = instagram[0]
             if party_logo:
                 item['party_logo'] = "http://congreso.es" + party_logo[0]
             if curriculum:
@@ -213,8 +202,7 @@ class MemberSpider(CrawlSpider):
                         item['active'] = False
 
             if extra_data:
-                web_data = Selector(response).xpath('//div[@class="webperso_dip"]/div[@class="'
-                                    'webperso_dip_parte"]/a/@href')
+                web_data = Selector(response).xpath('//div[@class="webperso_dip"]/div[@class="webperso_dip_parte"]/a/@href')
                 if web_data:
                     web = web_data.re('[http|https]*://.*')
                     if web:
@@ -222,13 +210,10 @@ class MemberSpider(CrawlSpider):
                 email = extra_data.re('mailto:[\w.-_]*@[\w.-_]*')
                 if email:
                     item['email'] = email[0].replace('mailto:', '')
-                #twitter = extra_data.re('[http|https]*://(?:twitter.com)/[\w]*')
-                #if twitter:
-                 #   item['twitter'] = twitter[0]
         congress = Congress()
         search = congress.getDeputy(name=item['name'])
         if not search:
-            congress.updateorinsertDeputy(type="insert",item=item)
+            congress.updateorinsertDeputy(type="insert", item=item)
         else:
             congress.updateorinsertDeputy(type="update", item=item)
         return item
