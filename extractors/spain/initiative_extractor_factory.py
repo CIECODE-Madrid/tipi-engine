92891e5e8a9f08f0ae8e37c0f8f0fb3a18e23243
David Hernandez <davidhernandezruiz@gmail.com>
2021-02-23 16:39:41 +0100

    Moves strategy map selection to the initiative_types list we already have.

--- extractors/spain/initiative_extractor_factory.py
+++ extractors/spain/initiative_extractor_factory.py
@@ -1,13 +1,27 @@
 from .initiative_extractors.initiative_extractor import InitiativeExtractor
+from .initiative_types import INITIATIVE_TYPES
+from urllib.parse import urlparse, parse_qs
 
 class InitiativeExtractorFactory:
     @staticmethod
-    def create(response, deputies, parliamentarygroups, places):
-        initiative_types_mapping = {}
-        response_type = ''
-        if response_type in initiative_types_mapping:
-            extractor = initiative_types_mapping.get(response_type)(response, deputies, parliamentarygroups, places)
-            return extractor
+    def get_type(response):
+        url = urlparse(response.request.path_url)
+        query = parse_qs(url.query)
+        return query.get('_iniciativas_id')[0].split('%')[0]
 
-        return InitiativeExtractor(response, deputies, parliamentarygroups, places)
+    @staticmethod
+    def get_type_extractor(response):
+        initiative_code = InitiativeExtractorFactory.get_type(response)
+
+        for initiative_type in INITIATIVE_TYPES:
+            if initiative_code == initiative_type.get('code') and "class" in initiative_type:
+                return initiative_type.get("class")
+
+        return InitiativeExtractor
+
+    @staticmethod
+    def create(response, deputies, parliamentarygroups, places):
+        extractor = InitiativeExtractorFactory.get_type_extractor(response)
+
+        return extractor(response, deputies, parliamentarygroups, places)
 
