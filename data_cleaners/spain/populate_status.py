7a0a67c7701b9019cff260cfe984a6497a670533
pr3ssh <x@pr3ssh.net>
2021-03-08 13:06:52 +0100

    Removes status map in data cleaner module for Spain

--- data_cleaners/spain/populate_status.py
+++ data_cleaners/spain/populate_status.py
@@ -4,13 +4,8 @@ import pymongo
 
 from tipi_data.models.initiative import Initiative
 
-from .status_map import STATUS_MAP
-
 
 class PopulateStatus:
 
     def populate(self):
-        for status_element in STATUS_MAP:
-            Initiative.all(
-                    __raw__=status_element['search']).update(
-                            status=status_element['status'])
+        pass
