e35c83da7b3ed5f92ff7717378adce2ff0b5fe45
pr3ssh <x@pr3ssh.net>
2021-03-24 12:06:25 +0100

    Removes tweetbot module

--- tweetbot/credentials.py
+++
@@ -1,7 +0,0 @@
-from os import environ as env
-
-
-CONSUMER_KEY = env.get('CONSUMER_KEY', '')
-CONSUMER_SECRET = env.get('CONSUMER_SECRET', '')
-ACCESS_TOKEN = env.get('ACCESS_TOKEN', '')
-ACCESS_SECRET = env.get('ACCESS_SECRET', '')
